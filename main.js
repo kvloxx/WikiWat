3ca2fced2ebab67fb9dc5150f93efdb4013069fa
kvloxx <kvloxx@gmail.com>
2016-03-18 21:45:14 -0400

    uploading page

---
+++ main.js
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>WikiWhy?</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js">
+	</script>
+	<script type="text/javascript" src="main.js"></script>
+</head>
+<body>
+	<div class="imgContainer"></div>
+</body>
+</html>
